413f58fce82fbb1aa451f1368756fcc8aa04d4db
liatsuki <lidia.duarte.goncalves@gmail.com>
2023-04-04 18:29:14 +0100

    aula 6

---
+++ Aulas/aula06a.py
@@ -0,0 +1,24 @@
+# Desafio 03
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+primeironumero = float(input('Primeiro número:'))
+segundonumero = float(input('Segundo número:'))
+soma = primeironumero + segundonumero
+print ('A soma é', soma)
+# Usei ChatGPT
+
+
+# Video 06 - Tipos Primitivos e Saída de Dados
+
+# Resolução do Guanabara:
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite um número:'))
+s = n1+n2
+print('A soma vale',s)
+
+# input() - usada para pedir ao usuário que digite um número
+# int() - usada para converter o valor digitado pelo usuário em um número inteiro
+
+# int() - números inteiros (ex: 7 -4 0 9875 - valor positivo, negativo ou nulo são valores inteiros)
+# float() - número reais ou números de pontos flutuante (ex: 4.5  0.076  -15.223  7.0)
+# bool() - valores lógicos ou bolivianos (só aceita valor verdadeiro "True" ou valor falso "False")
+# str() - valores carater ou strings (ex: 'Olá' '7.5' - é string porque está entre aspas; pode ter uma string vazia - '')
