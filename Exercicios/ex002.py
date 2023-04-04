f669a3803386f9338af6c4b5091d2e4cc20ecb8d
liatsuki <lidia.duarte.goncalves@gmail.com>
2023-04-04 16:10:27 +0100

    v6

--- Exercicios/ex002.py
+++ Exercicios/ex002.py
@@ -8,8 +8,13 @@ print('É um prazer te conhecer', nome,'!')
 nome=input('Digite seu nome: ')
 print('É um prazer te conhecer, {}!'.format(nome))
 
-# Ambos os códigos pedem para o usuário digitar o seu nome e, em seguida, imprimem uma mensagem de boas-vindas na tela com o nome que foi digitado.
+# Ambos os códigos pedem para o usuário digitar o seu nome e, em seguida,
+# imprimem uma mensagem de boas-vindas na tela com o nome que foi digitado.
 
-# A diferença entre os dois códigos é como eles inserem o nome na mensagem de boas-vindas. O primeiro código simplesmente adiciona o nome à mensagem usando vírgulas, enquanto o segundo código usa uma função chamada "format()" para adicionar o nome entre chaves na mensagem.
+# A diferença entre os dois códigos é como eles inserem o nome na mensagem de boas-vindas.
+# O primeiro código simplesmente adiciona o nome à mensagem usando vírgulas,
+# enquanto o segundo código usa uma função chamada "format()" para adicionar o nome entre chaves na mensagem.
 
-# A vantagem do segundo código é que ele é mais flexível e permite inserir várias variáveis e formatar mensagens mais complexas de uma maneira mais fácil de ler e entender. Mas, se você está escrevendo uma mensagem simples, o primeiro código pode ser mais fácil de usar.
+# A vantagem do segundo código é que ele é mais flexível e permite inserir várias variáveis
+# e formatar mensagens mais complexas de uma maneira mais fácil de ler e entender.
+# Mas, se você está escrevendo uma mensagem simples, o primeiro código pode ser mais fácil de usar.
