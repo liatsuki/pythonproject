5800cc497b6388ca35389cc7e8aa563738836c5f
liatsuki <lidia.duarte.goncalves@gmail.com>
2023-04-04 18:27:17 +0100

    script v1

--- Aulas/scripts-python/desafio02.py
+++ Aulas/scripts-python/desafio02.py
@@ -1,3 +1,7 @@
+# Video 04 - Primeiros comandos em Python3
+
+# Desafio 02
+# Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostra uma mensagem com uma data formatada
 dia = input ('Dia =')
 mes = input ('Mês =')
 ano = input ('Ano =')
