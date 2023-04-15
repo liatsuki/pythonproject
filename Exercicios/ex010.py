bea327b048170d04b4a2ffa56c5835ab23dcf95d
liatsuki <lidia.duarte.goncalves@gmail.com>
2023-04-15 22:27:59 +0100

    15042227

--- Exercicios/ex010.py
+++ Exercicios/ex010.py
@@ -1,14 +1,14 @@
 # Aula 07 - Desafio 010
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
 
-n1=float(input('Valor em euros: '))
-s=n1*1455.188
-print('O valor convertido para won é de {}'.format(s))
+n1 = float(input('Quanto dinheiro você tem na carteira? '))
+s = n1 / 1.09
+print('Com {:.2f}€ você pode comprar {:.2f}$.'.format(n1, s))
 
 
 #valor arrendondado - chatgpt
-n1 = float(input('Valor em Euros: '))
-won = n1 * 1455.188
-won_arredondado = round(won, 2)
-print('O valor convertido para Wons é de', won_arredondado)
+# n1 = float(input('Valor em Euros: '))
+# won = n1 * 1455.188
+# won_arredondado = round(won, 2)
+# print('O valor convertido para Wons é de', won_arredondado)
 
--- Exercicios/ex011.py
+++ Exercicios/ex011.py
@@ -2,9 +2,9 @@
 # Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m^2
 
 #Eu
-largura=float(input('Introduza a largura: '))
-altura=float(input('Introduza a altura: '))
+largura=float(input('Largura da parede: '))
+altura=float(input('Altura da parede: '))
 area=largura*altura
 quant_tinta=area/2
-print('Com uma largura de {} e com uma altura de {}, a área será de {}'.format(largura, altura, area))
-print('Quantidade de tinta necessária:', quant_tinta)
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m2'.format(largura, altura, area))
+print('Para pintar essa parede, você precisará de {}l de tinta'.format(quant_tinta))
