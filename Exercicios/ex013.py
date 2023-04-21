7aff207fca049648bb146bb91b5e882761e1faa1
liatsuki <lidia.duarte.goncalves@gmail.com>
2023-04-22 00:17:43 +0100

    22040017

--- Exercicios/ex013.py
+++ Exercicios/ex013.py
@@ -5,3 +5,8 @@
 n1=float(input('Salário: '))
 aum=n1+(n1*0.15)
 print('O salário de {}€ com um aumento de 15% de aumento é de {}€'.format(n1, aum))
+
+#Guanabara
+salário = float(input('Salário: '))
+novo = salário + (salário * 15 / 100)
+print('Um funcionário que ganhava {:.2f}€, com 15% de aumento, passa a receber {:.2f}€'.format(salário, novo))
