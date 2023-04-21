10b6a4129483a6df634817b0959925138439e055
liatsuki <lidia.duarte.goncalves@gmail.com>
2023-04-21 23:56:48 +0100

    21042356

--- Exercicios/ex012.py
+++ Exercicios/ex012.py
@@ -6,9 +6,14 @@ n1=float(input('Preço do produto: '))
 desc=n1-(n1*0.05)
 print('Novo preço com 5% de desconto: {}€'.format(desc))
 
+#Guanabara
+preço = float(input('Qual é o preço do produto? '))
+novo = n1 - (n1 * 5 / 100)
+print('O produto que custava {:.2f}€, na promoçao com desconto de 5% vai custar {:.2f}€'.format(preço, novo))
+
 
 #ChatGPT
-preco_produto = float(input("Digite o preço do produto: "))
-desconto = preco_produto * 0.05
-novo_preco = preco_produto - desconto
-print(f"O novo preço com 5% de desconto é {novo_preco:.2f}€")
+# preco_produto = float(input("Digite o preço do produto: "))
+# desconto = preco_produto * 0.05
+# novo_preco = preco_produto - desconto
+# print(f"O novo preço com 5% de desconto é {novo_preco:.2f}€")
